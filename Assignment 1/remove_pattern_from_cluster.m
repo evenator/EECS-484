434edc1c4429447a131dda62dd8d8e08fe60d981
Ed Venator <esv@case.edu>
2011-09-08 15:43:33 -0400

    Finished removing cluster. (untested)

--- Assignment 1/remove_pattern_from_cluster.m
+++ Assignment 1/remove_pattern_from_cluster.m
@@ -13,9 +13,8 @@
 current_cluster=pattern_assignments(ipat)
 if (current_cluster>0) %remove only if currently assigned
     pattern_assignments(ipat)=0; %unassign pattern ipat from any cluster
-    %FINISH THIS...
-    %UPDATE ASSOCIATED CLUSTER POPULATION, CLUSTER ATTRIBUTE and 
-    %CLUSTER CENTROID
- 
+    cluster_populations(current_cluster) = cluster_populations(current_cluster)-1;%decrement current cluster's population
+    cluster_attributes(current_cluster) = (cluster_attributes(current_cluster)*(cluster_populations(current_cluster)+1)-attributes(ipat))/cluster_populations(current_cluster)%recalculate cluster's average attribute
+    cluster_centroids(current_cluster,:)=cluster_centroids(current_cluster,:)*(cluster_populations(current_cluster)+1)/cluster_populations(current_cluster)-feature_scaled_vals(ipat,:)/cluster_populations(current_cluster); %recalculate cluster centroid
     cluster_rosters(current_cluster,ipat)=0; %remove this pattern from the current_cluster's roster
  end  %end if--which may complete addition to this cluster
