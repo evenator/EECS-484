e171adac5cdb315601646aa19330a9569395a909
Ed Venator <esv@case.edu>
2011-09-19 22:03:59 -0400

    Added exponential decay to the alpha value (as per book).

--- Assignment 2/alphafnc.m
+++ Assignment 2/alphafnc.m
@@ -5,14 +5,14 @@ function [alpha,radius]=alphafnc(i,j,ictr,jctr,time)
 dist = pdist([i, j; ictr, jctr], 'euclidean'); %uses Euclidean dist
 
 %Influence radius
-radius_init = 3;
-tau = 1000;
+radius_init = 4;
+tau = 1660;
 radius = radius_init * exp(-time/tau);
 %radius = radius_init; %*exp(-time/10000);% (1-time/100000);% * 1/(1+exp(time-1000));
 
 %alpha decays linearly w/ radius
 alpha_init = .1;
-alpha = alpha_init * exp(-dist^2/(2*radius^2));
+alpha = alpha_init * exp(-time/tau_2) * exp(-dist^2/(2*radius^2));
 %alpha = alpha_init/(1+exp(dist-radius));%* 1/(time/10000*(1+exp(dist-radius)));
 
 %return value of alpha=influence coefficient of current pattern vector on
