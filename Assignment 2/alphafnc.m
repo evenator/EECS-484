c2011b2d37c03fb11146f9b295e850c257f1da9a
Ed Venator <esv@case.edu>
2011-09-20 22:19:11 -0400

    Report looking good. Committing to do some final runs on the desktop, then submitting.

--- Assignment 2/alphafnc.m
+++ Assignment 2/alphafnc.m
@@ -6,15 +6,11 @@ dist = pdist([i, j; ictr, jctr], 'euclidean'); %uses Euclidean dist
 
 %Influence radius
 radius_init = 2;
-tau = 16000;
-radius = radius_init * exp(-time/tau);
+tau_r = 16000;
+radius = radius_init * exp(-time/tau_r);
 %radius = radius_init; %*exp(-time/10000);% (1-time/100000);% * 1/(1+exp(time-1000));
 
 %alpha decays linearly w/ radius
 alpha_init = .3;
-tau_2 = 50000;
-alpha = alpha_init * exp(-time/tau_2) * exp(-dist^2/(2*radius^2));
-%alpha = alpha_init/(1+exp(dist-radius));%* 1/(time/10000*(1+exp(dist-radius)));
-
-%return value of alpha=influence coefficient of current pattern vector on
-%cluster at location i,j.  Also return the influence radius, "radius"
+tau_a = 50000;
+alpha = alpha_init * exp(-time/tau_a) * exp(-dist^2/(2*radius^2));
