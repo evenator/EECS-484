c90cd9c7d507a7a430135359a595c5592b0ec0d4
Ed Venator <esv@case.edu>
2011-09-20 23:43:01 -0400

    Done with assignment 2. Report is done.

--- Assignment 2/alphafnc.m
+++ Assignment 2/alphafnc.m
@@ -5,12 +5,12 @@ function [alpha,radius]=alphafnc(i,j,ictr,jctr,time)
 dist = pdist([i, j; ictr, jctr], 'euclidean'); %uses Euclidean dist
 
 %Influence radius
-radius_init = 2;
-tau_r = 16000;
+radius_init = 3;
+tau_r = 16600;
 radius = radius_init * exp(-time/tau_r);
 %radius = radius_init; %*exp(-time/10000);% (1-time/100000);% * 1/(1+exp(time-1000));
 
 %alpha decays linearly w/ radius
-alpha_init = .3;
+alpha_init = .2;
 tau_a = 50000;
 alpha = alpha_init * exp(-time/tau_a) * exp(-dist^2/(2*radius^2));
