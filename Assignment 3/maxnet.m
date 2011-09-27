a6c796d685d810888d03cd36616e546884de47b0
Ed Venator <esv@case.edu>
2011-09-27 19:09:00 -0400

    Rewrote loop conditions to run until maximized. Removed a lot of outputs and plots to only show final result. Code is now ready to be automated.

--- Assignment 3/maxnet.m
+++ Assignment 3/maxnet.m
@@ -8,8 +8,8 @@ clear all %delete any old variables still hanging around
 figure(1) %clear figure 1 for display
 clf %clear the current figure
 
-eps=-0.02; %choose mutual inhibition strength
-node_count = 5; %choose a number of interconnected nodes
+eps=-01 %choose mutual inhibition strength
+node_count = 5 %choose a number of interconnected nodes
 W = eps * ones(node_count, node_count) + (1 - eps) * eye(node_count)
 
 %initialize a set of outputs:
@@ -25,34 +25,32 @@ for i=1:node_count
     plot(time,sigma_vec(i))
 end
 
-while 1>0   %infinite loop; requires user to hit ctl-C to break out of loop--else revise termination condition
-    time=time+1; %implicitly sets the time delay of each neuron (stimulus to response) to be 1 unit
-    time_vec=[time_vec,time]; %add an element to the time vector
+while length(find(sigma_vec))>1
+    time = time+1; %implicitly sets the time delay of each neuron (stimulus to response) to be 1 unit
+    time_vec = [time_vec,time]; %add an element to the time vector
  
-    net_inputs = W*sigma_vec %make sure you understand what this means and why it is true
+    net_inputs = W*sigma_vec; %make sure you understand what this means and why it is true
 
     %for each neuron, compute firing rate as a function of net voltage at soma
     for i=1:node_count
-        sigma_vec(i)=activation_fnc(net_inputs(i)); %outputs follow from inputs and activation fnc
+        sigma_vec(i) = activation_fnc(net_inputs(i)); %outputs follow from inputs and activation fnc
     end
-    sigma_vec %display the output values
-    sigma_history=[sigma_history,sigma_vec]; %keep track of this for plotting history
- 
-    %rest of this is just plotting--overwrite the figure to show current status
-    hold off %clear the figure
-    clf
-    hold all %allows adding multiple lines to the plot
-    for i=1:node_count
-        y_vec=sigma_history(i,:); %strip off i'th row of neural responses (for i'th neuron's responses)
-        plot(time_vec,y_vec) %plot this neural response vs time
-    end
-    title('Maxnet time history of outputs')
-    xlabel('time')
-    ylabel('sigma outputs')
-    % done with plot
-    pause %for single-stepping; user must hit "enter" to continue with next iteration
+    sigma_vec; %display the output values
+    sigma_history = [sigma_history,sigma_vec]; %keep track of this for plotting history
 end
 
-
-    
-        
+time
+sigma_vec
+ 
+%rest of this is just plotting--overwrite the figure to show current status
+hold off %clear the figure
+clf
+hold all %allows adding multiple lines to the plot
+for i=1:node_count
+    y_vec=sigma_history(i,:); %strip off i'th row of neural responses (for i'th neuron's responses)
+    plot(time_vec,y_vec) %plot this neural response vs time
+end
+title('Maxnet time history of outputs')
+xlabel('time')
+ylabel('sigma outputs')
+% done with plot
