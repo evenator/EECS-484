68742284f476d34fc990b75837342a3ff0a401b7
Ed Venator <esv@case.edu>
2011-09-28 09:03:28 -0400

    Modified boundary conditions to only try eps that are guaranteed to work (eps_min = -1/(max_nodes-1)); added averaging of ten trials for each eps/node_count pair; took a bunch of datapoints.

--- Assignment 3/automate_maxnet.m
+++ Assignment 3/automate_maxnet.m
@@ -1,12 +1,23 @@
 max_nodes = 100;
 eps_step = .001;
 results = zeros(ceil(2 / eps_step), 4);
+iterations = 10;
 
 row_count = 1;
+time_temp = zeros(iterations,1);
+success_temp = zeros(iterations,1);
 
 for node_count = 2:max_nodes
-    for eps = (-1/node_count):eps_step:-eps_step
-        auto_maxnet;
+    for eps = (-1/(node_count-1)):eps_step:-eps_step
+        for i = 1:iterations
+            auto_maxnet;
+            time_temp(i) = time;
+            success_temp(i) = length(max_node);
+        end
+        %auto_maxnet;
+        %success = length(max_node);
+        time = mean(time_temp);
+        success = max(success_temp);
         results(row_count,:) = [eps, node_count, time, length(max_node)];
         row_count = row_count + 1;
     end
