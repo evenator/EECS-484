2bf54518b6d41ea18bf3f0b639b3d8ebd19b1ddc
Ed Venator <esv@case.edu>
2011-10-11 19:37:44 -0400

    Fixed matrix dim errors in compute_W_derivs.m. Can now accurately compute dWkj.

--- Assignment 4/compute_W_derivs.m
+++ Assignment 4/compute_W_derivs.m
@@ -3,8 +3,8 @@ function  [dWkj,dWji] = compute_W_derivs(Wji,Wkj,training_patterns,targets)
 dWji=0*Wji; %sets dimensions of output matrices
 dWkj=0*Wkj;
 
-[P,K] = size(targets); %P training patterns, K-dimensional output
-[J,I] = size(Wji) %J interneurons, I-dimensional input patterns
+[P,K] = size(targets) %P training patterns, K-dimensional output
+[J,I] = size(Wji); %J interneurons, I-dimensional input patterns
 
 %loop over all input patterns and compute influence of each pattern on
 %dE/dw
@@ -24,7 +24,7 @@ for p=1:P %make the P loop the outer loop, since need to re-use results of
     for k=1:K %compute influence w/rt output node k
         err_k = outputk(k)-targets(p,k); %error for output k vs target k--will need this
         gprime_k = outputk(k) * (1-outputk(k)); %slope of activation fnc for neuron k for pattern p
-        dWkj(k) = dWkj(k) + err_k * gprime_k * outputk;
+        dWkj(k,:) = dWkj(k,:) + err_k * gprime_k * outputj';
         % will also need gprime_j in an inner loop for dE/dwji terms
         for j = 1:J %Compute influence w/rt interneuron j
             gprime_j = outputj(j) *( 1-outputj(j));
