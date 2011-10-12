ee3bb065327f643271e32679f2fb9680270594a5
Ed Venator <esv@case.edu>
2011-10-12 00:01:50 -0400

    Got toolbox working and added node count stepping automation.

--- Assignment 4/nntbox_example.m
+++ Assignment 4/nntbox_example.m
@@ -5,15 +5,17 @@
 % for output layer
 % put training patterns in rows: P=4 patterns
 p1=[0,0];
-t1=[0];
+t1=0;
 p2=[1,1];
-t2=[0];
+t2=0;
 p3=[1,0];
-t3=[1];
+t3=1;
 p4=[0,1];
-t4=[1];
+t4=1;
 training_patterns=[p1;p2;p3;p4];  %store pattern inputs as row vectors in a matrix
+
 targets=[t1;t2;t3;t4];  % should have these responses; rows correspond to input pattern rows
+
 ndim_inputs=2; %2D patterns--not counting bias
 nnodes_layer1=4; %try this many interneurons--not including bias
 nnodes_layer2=1; %single output
@@ -21,34 +23,44 @@ nnodes_layer2=1; %single output
 ranges = [0 1; 0 1]; %define 2-D input patterns, values range 0 to 1
 
 %EDIT THIS: watch out for presumption of rows vs columns for input matrices
-net = newff(...) % fill in necessary arguments to build a network object
+net = feedforwardnet(nnodes_layer1); % fill in necessary arguments to build a network object
+
 net=init(net); %init all W values; READ ABOUT INIT(NET)
 
 %some debug--probe values of the new net object
-net.layerConnect % 1 in (i,j) if there is a connection from layer j to layer i
-net.outputConnect % shows which layers can produce outputs: [0 1]==> second layer only has outputs
-net.targetConnect % shows only second-layer outputs have targets
-net.numOutputs  % single output
-net.LW{2,1} %display weights onto layer 2 from layer 1
-net.layers{1}.size
-net.layers{2}.size
-net.layerWeights{2,1}.size
-net.inputs{1}.size %dimension of input patterns
+%net.layerConnect % 1 in (i,j) if there is a connection from layer j to layer i
+%net.outputConnect % shows which layers can produce outputs: [0 1]==> second layer only has outputs
+%net.targetConnect % shows only second-layer outputs have targets
+%net.numOutputs  % single output
+%net.LW{2,1} %display weights onto layer 2 from layer 1
+%net.layers{1}.size
+%net.layers{2}.size
+%net.layerWeights{2,1}.size
+%net.inputs{1}.size %dimension of input patterns
 %net.inputs{2}.size % there are no inputs to layer 2
 
 %EDIT THIS; 
-[net,tr,Y,E,Pf,Af] = train(...)  %do NN training--fill in correct args
+net.divideFcn = 'dividetrain';
+net.trainFcn = 'trainlm';
+%net.trainFcn = 'traingd';
+%net.trainParam.lr=.5;
+net.layers{1}.transferFcn = 'tansig';
+net.layers{2}.transferFcn = 'purelin';
+net
+[net,tr,Y,~,~,~] = train(net, training_patterns', targets');  %do NN training--fill in correct args
 
 %plot out the result--create a new set of stimulus patterns at regular
 %intervals and simulate network
-evalpats=[];
-for (i=0:0.1:1)
-    for(j=0:0.1:1)
-     evalpats=[evalpats,[i;j]]
+evalpats = zeros(2,121);
+count = 1;
+for i=0:0.1:1
+    for j=0:0.1:1
+     evalpats(:,count)=[i;j];
+     count = count +1;
     end
 end
-xvals=[0:0.1:1] %same stimulus patterns represented as vectors
-yvals=[0:0.1:1]
+xvals = 0:0.1:1; %same stimulus patterns represented as vectors
+yvals = 0:0.1:1;
 
 %SIMULATE the network with new input patterns--READ ABOUT "sim()"
 [simOutputs,Pf,Af,E,perf] = sim(net,evalpats); %simulate the entire set
@@ -56,8 +68,8 @@ yvals=[0:0.1:1]
 %pack the outputs into an array for surface plotting--only works for 2-D in, 1-D out
 Z=zeros(11,11);
 p=1;
-for (i=1:11)
-    for(j=1:11)
+for i=1:11
+    for j=1:11
      Z(i,j)=simOutputs(p);
      p=p+1;
     end
---
+++ Assignment 4/ps4_fdfwd_net_main_node_stepping.m
@@ -0,0 +1,74 @@
+for num_nodes = 2:20
+%2-layer fdfwd network to be trained w/ custom BP
+%for xor, define 3 inputs, including bias (I=3),  J interneurons (incl bias), and 1 output
+%choose logsig (sigmoid) nonlinear activation fnc 
+% put training patterns in rows: P=4 patterns
+p1=[1,0,0];
+t1=[0];
+p2=[1,1,1];
+t2=[0];
+p3=[1,1,0];
+t3=[1];
+p4=[1,0,1];
+t4=[1];
+training_patterns=[p1;
+    p2;
+    p3;
+    p4];  %store pattern inputs as row vectors in a matrix
+targets=[t1;
+    t2;
+    t3;
+    t4];  % should have these responses; rows correspond to input pattern rows
+ndim_inputs=3; %2D patterns plus bias
+nnodes_layer1=num_nodes; %try this many interneurons--including bias virtual neuron; experiment with this number
+nnodes_layer2=1; %single output
+%weights from pattern inputs to layer-1 (interneurons)
+%initialize weights to random numbers, plus and minus--may want to change
+%range; first row has dummy inputs, since first interneuron is unchanging bias node
+W1p = (2*rand(nnodes_layer1,ndim_inputs)-1); %matrix is nnodes_layer1 x ndim_inputs
+
+%weights from interneurons (and bias) to output layer 
+W21 = (2*rand(nnodes_layer2,nnodes_layer1)-1); %includes row for bias inputs
+
+%evaluate networkover a grid of inputs and plot using "surf"; 
+%works only in special case:  assumes inputs are 2-D and range from 0 to 1 
+ffwd_surfplot(W1p,W21);
+eps=0.95; % tune this value; may also want to vary this during iterations
+iteration=0;
+%BP:
+iter1k=0;
+while (iteration<1000) % infinite loop--ctl-C to stop; edit this to run finite number of times
+    %compute all derivatives off error metric w/rt all weights; put these
+    %derivatives in matrices dWkj and dWji
+    iteration=iteration+1;
+    %WRITE THIS FUNCTION...
+    [dWkj,dWji] = compute_W_derivs(W1p,W21,training_patterns,targets);
+
+    %DEBUG: uncomment the following and prove that your compute_W_derivs
+    %yields the same answer as numerical estimatesfor dE/dW
+    %comment out to run faster, once debugged
+    %dWkj %display derivative computation
+    %est_dWkj= numer_est_Wkj(W1p,W21,training_patterns,targets) %and numerical estimate
+    %dWji %display sensitivities dE/dwji
+    %est_dWji=numer_est_Wji(W1p,W21,training_patterns,targets) %and numerical estimate
+
+    %use gradient descent to update all weights:
+    W1p=W1p-eps*dWji;
+    W21=W21-eps*dWkj;
+    
+    %optional debug: plot out incremental progress every plot_iter iterations
+    plot_iter=100;
+    %iter1k=iteration;
+    if (iteration-iter1k>plot_iter)
+        %ffwd_surfplot(W1p,W21);
+        iter1k=iteration;
+        iteration
+        %pause(0.5)
+        rmserr = err_eval(W1p,W21,training_patterns,targets)
+    end
+
+end
+  ffwd_surfplot(W1p,W21); %print out final plot, if loop terminates
+
+
+
