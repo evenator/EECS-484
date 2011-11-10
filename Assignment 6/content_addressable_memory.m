a29996e7686b240ba7607b0fecec8f97fc00298c
Ed Venator <esv@case.edu>
2011-11-09 20:25:01 -0500

    updateRandomNode() written; basic recollection now working.

--- Assignment 6/content_addressable_memory.m
+++ Assignment 6/content_addressable_memory.m
@@ -1,8 +1,8 @@
-%content_addressable_memory.m: 
+%content_addressable_memory.m:
 % read in files to memorize and cues to recover memories
-%note: put bitmaps in same directory as this file
+%note: put bitmaps in /images directory
 
-A = imread('happyworld.bmp'); % example file to read in.
+A = imread('images/happyworld.bmp'); % example file to read in.
 
 [nrows,ncols]=size(A); %get bitmap dimensions;
                                          % need to remember these to convert vector back to matrix
@@ -16,7 +16,7 @@ MatrixMem=Avec*Avec'; %outer product
 
 % read in some more images...
 % experiment with how many and which images to read in...
-A = imread('clubspade.bmp')
+A = imread('images/clubspade.bmp')
 Avec = matrix2vec(A);
 M=Avec*Avec'; % again, consider partial or full suppression of diagonal
 MatrixMem=MatrixMem+M; %include more memories by merely adding them into matrix
@@ -32,17 +32,20 @@ MatrixMem=MatrixMem+M; %include more memories by merely adding them into matrix
 size(MatrixMem) % debug--should be 512x512 for these examples
 
 fname = input('enter filename of cue: ','s'); %prompt for a cue (flawed memory)
-Aerr=imread(fname);
+Aerr=imread(['images/' fname]);
 
 Avec_err=matrix2vec(Aerr);
 AVecRecall=Avec_err;
 
 while 1>0 % infinite loop...
-  nupdates = input('enter number of nodes to update:' )
+  nupdates = input('enter number of nodes to update:' );
+  if nupdates <=0
+      break
+  end;
   numchanges=0;
   for iupdate=1:nupdates
     [AVecRecall,delta] = updateRandomNode(MatrixMem,AVecRecall); %WRITE THIS FUNCTION
-    numchanges=numchanges+delta;
+    numchanges = numchanges+delta;
   end
    numchanges %print out how many nodes were changed
    AmatCorrected=vec2matrix(AVecRecall,nrows,ncols); %convert vector back to matrix
--- Assignment 6/updateRandomNode.m
+++ Assignment 6/updateRandomNode.m
@@ -1,5 +1,8 @@
    % fnc to correct nodes in matrix memory
    %YOU MUST WRITE THIS FUNCTION
-   function [vec,delta] = updateRandomNode(MatrixMem,vec)
-     delta=0; %count how many nodes change their values
+function [vec,delta] = updateRandomNode(MatrixMem,vec)
+    node_index = random('unid', length(vec));
+    delta = vec(node_index);
+    vec(node_index) = sign(MatrixMem(node_index,:) * vec);
+    delta = abs(delta - vec(node_index))/2;
     
