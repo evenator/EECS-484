f6b201d90b07669435e32c2bb1e1d0bb3bd9d00e
Ed Venator <esv@case.edu>
2011-11-28 18:52:06 -0500

    F_wji should be done.

--- Assignment 8/compute_F_wji.m
+++ Assignment 8/compute_F_wji.m
@@ -3,5 +3,11 @@ function [F_wji] = compute_F_wji(sigma_history,F_u)
 [Nneurons, T_time_steps] = size(F_u);
 F_wji = zeros(Nneurons,Nneurons); %create holder for output with correct dimensions
 
-%compute sum of F_uvals terms over time:
-%DO SOMETHING REAL HERE
+%This is really inefficient, but should work
+for j = 1:Nneurons
+    for i = 1:Nneurons
+        for t = 1:(T_time_steps-1)
+            F_wjk(j,i) = F_u(j,t+1)*sigma_history(t);
+        end
+    end
+end
