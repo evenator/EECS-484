6f261a7e3ee78f72a491be02094577a6724bd6bc
Ed Venator <esv@case.edu>
2011-11-28 19:01:19 -0500

    Minor fix to F_u; should be done.

--- Assignment 8/compute_F_u.m
+++ Assignment 8/compute_F_u.m
@@ -1,9 +1,9 @@
 %function compute_F_u computes ordered derivatives at time t
 %with respect to u's
-function  F_u_vec = compute_F_u(F_o,gprimes,t);
+function  F_u_vec = compute_F_u(F_o,gprimes,t)
 [Nneurons, T_time_steps] = size(F_o);
 
 F_u_vec = zeros(Nneurons,1); %initialize vector of dE/du(t) to zeros
 for i=3:Nneurons %only care about neurons 3 through final
-    F_u_vec(i) = gprimes(i)*F_o(i);
+    F_u_vec(i) = gprimes(i)*F_o(i,t);
 end
