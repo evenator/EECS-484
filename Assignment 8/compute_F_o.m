7230f084de8af84375dcfb52a5cecd7971c43195
Ed Venator <esv@case.edu>
2011-11-28 21:31:41 -0500

    Fixed a derp in the calculation of F_u. The derp hunt continues.

--- Assignment 8/compute_F_o.m
+++ Assignment 8/compute_F_o.m
@@ -10,10 +10,16 @@ F_o_vec = zeros(Nneurons,1);
 % for t< final time, dE/dsigma(t) depends on dE/du(t+1)
 if t<T_time_steps
     F_u_vec = F_u(:,t+1);
-    F_o_vec = errs(t)*ones(4,1) + W*F_u_vec;
+    for i = Nneurons:-1:3
+        for j = (i+1):Nneurons
+            F_o_vec(i) = F_o_vec(i) + W(i,j)*F_u_vec(j);
+        end
+    end
 end
 
 F_o_vec(1)=0; %don't change bias = neuron 1
 F_o_vec(2)=0; %don't change input = neuron 2
 %for output node, neuron 3, add in influences of partial deriv of errors w/rt sigmas at time t
-%FIX THIS
+for i = Nneurons:-1:3
+    F_o_vec(i) = F_o_vec(i) + errs(t);
+end
--- Assignment 8/compute_F_u.m
+++ Assignment 8/compute_F_u.m
@@ -5,5 +5,5 @@ function  F_u_vec = compute_F_u(F_o,gprimes,t)
 
 F_u_vec = zeros(Nneurons,1); %initialize vector of dE/du(t) to zeros
 for i=3:Nneurons %only care about neurons 3 through final
-    F_u_vec(i) = gprimes(i)*F_o(i,t);
+    F_u_vec(i) = gprimes(i,t)*F_o(i,t);
 end
