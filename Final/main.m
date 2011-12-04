84351b1d891278c3edd9e8b37645b3cc77d3d529
Ed Venator <esv@case.edu>
2011-12-04 10:24:10 -0500

    More changes. Next up: move to HSV.

--- Final/main.m
+++ Final/main.m
@@ -2,13 +2,16 @@ clear all
 clc
 
 %Begin Tunable Parameters
-n_samples = 1000; %Number of pixels to sample in each image
+n_samples = 10000; %Number of pixels to sample in each image
 over_sample = 5; %How many times to sample from each image
 
 %End Tunable Parameters
 
 %Load Data and Sample From Images
 [patterns, targets] = load_training_bananas(n_samples,over_sample);
+
+%Create Validation Vectors by Sampling Yet Again
+[validation, val_targets] = load_training_bananas(n_samples,1);
 
 %Classify with Self-Ordered Mapping
 som_bananify
--- Final/som_bananify.m
+++ Final/som_bananify.m
@@ -1,18 +1,33 @@
 global r tau_r alpha0 tau_alpha;
 
 %Tunable Parameters
-num_bins = 10;
-training_time = 1000;
-r = 3; %Influence Radius
-tau_r = 1000; %Influence Radius Decay Time Constant
-alpha0 = .2; %Influence at distance=0
-tau_alpha = 1000; %Influence Decay Time Constant
+num_bins = 5;
+training_time = 10000;
+r = .1; %Influence Radius
+tau_r = 100; %Influence Radius Decay Time Constant
+alpha0 = .75; %Influence at distance=0
+tau_alpha = 100; %Influence Decay Time Constant
 %End Tunable Parameters
 
 [n_pats pat_length] = size(patterns);
 
-%Create bins
-bins = random('unid', 100, [num_bins,pat_length] );
+%Create bins and initialize with random patterns (initializing randomly
+%results in all patterns being binned together)
+bins = zeros(num_bins, pat_length);
+pat_list = zeros(num_bins,1);
+%pick a random pattern number:
+for i = 1:num_bins
+    %Randomly choose a pattern
+    ipat = random('unid',n_pats);
+    %If the pattern is already in a bin, try again
+    while ~isempty(find(pat_list==ipat ))
+        ipat = random('unid',n_pats);
+    end
+    %Add the pattern to the list
+    pat_list(i) = ipat;
+    %Initialize the bin
+    bins(i,:) = ipat;
+end
 
 %Normalize Bin Feature Vectors
 for i = 1:num_bins
@@ -36,3 +51,13 @@ while (time<training_time)
         bins(i,:) = bins(i,:) ./ norm(bins(i,:));
     end
 end
+
+%Assign patterns to bins
+[num_validation_pats ~] = size(validation);
+bin_list = zeros(num_validation_pats, 1);
+for i = 1:num_validation_pats
+    bin_list(i) = som_find_closest_bin(validation(i,:),bins);
+end
+
+%Debug results
+cat(2,bin_list,val_targets)
